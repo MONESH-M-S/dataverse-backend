310f241c3ab2d91389115c80425f15da42ecb450
Lokesh Kumar <lokkumar.ext@deloitte.com>
2023-02-22 19:49:05 +0530

    Removed unnecessary files

--- src/models/library.models.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/sequelize.config");
-
-const LibraryModel = sequelize.define('Library', {
-    name: DataTypes.STRING,
-    athor: DataTypes.STRING,
-});
-
-module.exports = LibraryModel
