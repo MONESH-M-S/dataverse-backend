f1eef2605bf1b17c209d796ed54935d97fde4684
Krishna Uppili <krishna@rootquotient.com>
2023-03-09 16:34:25 +0530

    Change sequelize logic in root model

--- models/index.js
+++ models/index.js
@@ -1,50 +1,63 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const process = require("process");
 const basename = path.basename(__filename);
 
-const { DB_NAME, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DIALECT } = process.env
+const { DB_NAME, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DIALECT } = process.env;
 
 let encodedPassword = DB_PASSWORD;
-let bufferData = new Buffer.from(encodedPassword, 'base64');
-let decodedPassowrd = bufferData.toString('ascii');
+let bufferData = new Buffer.from(encodedPassword, "base64");
+let decodedPassowrd = bufferData.toString("ascii");
 
 const config = {
-  "username": DB_USERNAME,
-  "password": decodedPassowrd,
-  "database": DB_NAME,
-  "host": DB_HOST,
-  "dialect": DB_DIALECT
-}
+  username: DB_USERNAME,
+  password: decodedPassowrd,
+  database: DB_NAME,
+  host: DB_HOST,
+  dialect: DB_DIALECT,
+};
 
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+const sequelize = new Sequelize({
+  dialect: "mssql",
+  dialectOptions: {
+    authentication: {
+      type: "azure-active-directory-msi-app-service",
+    },
+  },
+  host: "bieno-da08-d-904380-unilevercom-sql-01.database.windows.net",
+  database: "bieno-da08-d-904380-unilevercom-sqldb-01",
+  port: 1433,
+});
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
+fs.readdirSync(__dirname)
+  .filter((file) => {
     return (
-      file.indexOf('.') !== 0 &&
+      file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
