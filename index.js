d339458809ea6903b21b6617671131439c67ee1e
Krishna Uppili <krishna@rootquotient.com>
2023-03-09 14:12:57 +0530

    Package: Add MSSQL package for managed identity auth

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const env = process.env.NODE_ENV
-console.log("Environment is ", env)
+const env = process.env.NODE_ENV;
+console.log("Environment is ", env);
 
 switch (env) {
   case "test":
-    require('custom-env').env('test')
+    require("custom-env").env("test");
     break;
 
   default:
-    require('custom-env').env()
+    require("custom-env").env();
 }
 
 const express = require("express");
@@ -23,8 +23,8 @@ const server = http.createServer(app);
 
 const port = process.env.PORT || 3000;
 
-const smartMappingRoutes = require("./src/routes/smartMapping.routes")
-const metaRoutes = require("./src/routes/meta.routes")
+const smartMappingRoutes = require("./src/routes/smartMapping.routes");
+const metaRoutes = require("./src/routes/meta.routes");
 
 app.use(bodyParser.json());
 app.use(
@@ -40,6 +40,36 @@ app.use("/meta", metaRoutes);
 // Handling Errors message
 app.use(joiErrorHandlerMiddleware);
 app.use(errorHandlerMiddleware);
+
+const sql = require("mssql");
+
+const config = {
+  server: "tcp:bieno-da08-d-904380-unilevercom-sql-01.database.windows.net",
+  port: 1433,
+  database: "bieno-da08-d-904380-unilevercom-sqldb-01",
+  authentication: {
+    type: "azure-active-directory-msi-app-service",
+  },
+  options: {
+    encrypt: true,
+  },
+};
+
+console.log("Starting...");
+connectAndQuery();
+
+async function connectAndQuery() {
+  try {
+    var poolConnection = await sql.connect(config);
+
+    console.log("Reading rows from the Table...", poolConnection);
+
+    poolConnection.close();
+  } catch (err) {
+    console.log("Error occured");
+    console.error(err.message);
+  }
+}
 
 server.listen(port, "0.0.0.0", () => {
   console.log(`App listening at http://localhost:${port}`);
