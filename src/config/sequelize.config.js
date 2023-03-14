abdaf0dc60480ec79f321ff9587b33af01cc5e1b
Lokesh Kumar <lokkumar.ext@deloitte.com>
2023-03-14 17:53:19 +0530

    Updated the config

--- src/config/sequelize.config.js
+++ src/config/sequelize.config.js
@@ -5,6 +5,16 @@ const config = require('../../config/config')[env];
 console.log("Env is ", env)
 console.log("Config is ", config)
 
-const sequelize = new Sequelize(config);
+const sequelize = new Sequelize({
+    dialect: "mssql",
+    dialectOptions: {
+        authentication: {
+            type: "azure-active-directory-msi-app-service",
+        },
+    },
+    host: "bieno-da08-d-904380-unilevercom-sql-01.database.windows.net",
+    database: "bieno-da08-d-904380-unilevercom-sqldb-01",
+    port: 1433
+});
 
 module.exports = sequelize;
