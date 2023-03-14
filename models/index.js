dd31567bb571c9d64e91ec83c30380c742363e85
Lokesh Kumar <lokkumar.ext@deloitte.com>
2023-03-14 18:00:42 +0530

    Added hardcoded config in models/index.js

--- models/index.js
+++ models/index.js
@@ -10,7 +10,17 @@ const config = require(__dirname + '/../config/config.js')[env];
 
 console.log("Environment is ", env)
 
-const sequelize = new Sequelize(config);
+const sequelize = new Sequelize({
+  dialect: "mssql",
+  dialectOptions: {
+    authentication: {
+      type: "azure-active-directory-msi-app-service",
+    },
+  },
+  host: "bieno-da08-d-904380-unilevercom-sql-01.database.windows.net",
+  database: "bieno-da08-d-904380-unilevercom-sqldb-01",
+  port: 1433
+});
 
 fs.readdirSync(__dirname)
   .filter((file) => {
