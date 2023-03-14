4d67d65adf083a59c75cbb172996a14e67c29d84
Lokesh Kumar <lokkumar.ext@deloitte.com>
2023-03-14 17:10:44 +0530

    Updated config file

--- config/config.js
+++ config/config.js
@@ -7,11 +7,15 @@ const decodedPassowrd = bufferData.toString("ascii");
 
 module.exports = {
   development: {
-    username: DB_USERNAME,
-    password: decodedPassowrd,
-    database: DB_NAME,
-    host: DB_HOST,
-    dialect: DB_DIALECT,
+    dialect: "mssql",
+    dialectOptions: {
+      authentication: {
+        type: "azure-active-directory-msi-app-service",
+      },
+    },
+    host: "bieno-da08-d-904380-unilevercom-sql-01.database.windows.net",
+    database: "bieno-da08-d-904380-unilevercom-sqldb-01",
+    port: 1433,
   },
   test: {
     username: DB_USERNAME,
