5e09b151272011c0c3ee7e1999c4ea5d9e7c5143
Leo <lokesh.kumar@rootquotient.com>
2023-03-16 23:54:01 +0530

    Added db connectivity with the info schema

--- src/models/loadLog.model.js
+++ src/models/loadLog.model.js
@@ -1,16 +1,25 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/sequelize.config");
 
-const LoadLogModel = sequelize.define('load_log', {
-    PIPELINE_RUN_ID: DataTypes.STRING,
-    LOAD_DESC: DataTypes.TEXT,
-    LOAD_START_TIME: DataTypes.STRING,
-    LOAD_END_TIME: DataTypes.STRING,
+const LoadLogModel = sequelize.define('LoadLog', {
+    LogId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    PIPELINERUNID: DataTypes.STRING,
+    LOADDESC: DataTypes.TEXT,
+    LOADSTARTTIME: DataTypes.STRING,
+    LOADENDTIME: DataTypes.STRING,
     SOURCE: DataTypes.STRING,
-    FILE_NAME: DataTypes.STRING,
-    FILE_LAST_MODIFIED_DATE: DataTypes.STRING,
-    PIPELINE_STATUS: DataTypes.STRING,
-    RUNNING_USER: DataTypes.STRING,
+    FILENAME: DataTypes.STRING,
+    FILELASTMODIFIEDDATE: DataTypes.STRING,
+    PIPELINESTATUS: DataTypes.STRING,
+    RUNNINGUSER: DataTypes.STRING,
+}, {
+    tableName: 'LoadLog',
+    schema: 'info',
+    timestamps: false,
 });
 
 module.exports = LoadLogModel
