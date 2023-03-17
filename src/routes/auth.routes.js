4637ca7f4587abfb8fb8a6c7e1b6b737099ca287
Lokesh Kumar <lokkumar.ext@deloitte.com>
2023-03-17 11:26:16 +0530

    Modified the route method

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { fetchKeyVaultData, fetchAuthToken } = require("../controllers/authController");
 const router = express.Router();
 
-router.get("/auth", fetchAuthToken);
+router.post("/auth", fetchAuthToken);
 router.get("/vault", fetchKeyVaultData);
 // router.get("/list", fetchKeyVaultKeysList);
 
