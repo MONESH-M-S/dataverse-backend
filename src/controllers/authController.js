0bc6e1da1215fe6413f0c191a7545b36546f49ad
Lokesh Kumar <lokkumar.ext@deloitte.com>
2023-03-17 10:38:54 +0530

    Updated the key vault name

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,7 +10,7 @@ const fetchKeyVaultData = async (req, res, next) => {
 
     try {
         console.log("Inside key vault method")
-        const keyVaultName = 'bnlwe-da02-b-902592-kv01';
+        const keyVaultName = 'bnlwe-da02-b-904380-KV01';
         const KVUri = `https://${keyVaultName}.vault.azure.net`;
 
         const credential = new DefaultAzureCredential();
