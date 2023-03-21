13bc1b8d26410ceab232af5f6a029cb1ad8af3bd
Leo <lokesh.kumar@rootquotient.com>
2023-03-21 13:17:44 +0530

    Added additional route

--- src/routes/sso.routes.js
+++ src/routes/sso.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { fetchAuthToken } = require("../controllers/authController");
+const { fetchAuthToken, generateAuthToken } = require("../controllers/authController");
 const router = express.Router();
 
 router.get("/", fetchAuthToken);
+router.get("/get-token", generateAuthToken);
+
 
 module.exports = router;
