1d03951864ec85a99d917279746eba556300ac36
Leo <lokesh.kumar@rootquotient.com>
2023-04-11 00:54:07 +0530

    Fix the validation issue in adding target column

--- src/schema/addTarget.schema.js
+++ src/schema/addTarget.schema.js
@@ -1,8 +1,8 @@
 const Joi = require("joi");
 
 module.exports = Joi.array().items({
-    fileName: Joi.string().required(),
-    id: Joi.string().required(),
+    id: Joi.number().optional(),
+    key: Joi.number().optional(),
     target: Joi.string().required(),
     source: Joi.string().allow("")
 })
