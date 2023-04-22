b1eb0e4b35cf838fcdb8973d1ec9ac2159c9dc45
Dinesh <dineshkumar.p@unilever.com>
2023-04-22 10:35:43 +0530

    Added Keyvault for Client Secret msalconfig

--- src/config/msal.config.js
+++ src/config/msal.config.js
@@ -1,37 +1,62 @@
 const msal = require("@azure/msal-node");
+const { SecretClient } = require("@azure/keyvault-secrets");
+const { DefaultAzureCredential } = require("@azure/identity");
+const env = process.env.APP_ENVIRONMENT;
 
-const msalConfig = {
-    tenant: process.env.TENENT_ID,
-    auth: {
-        clientId: process.env.CLIENT_ID, // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
-        authority: `https://login.microsoftonline.com/${process.env.TENENT_ID}`, // Full directory URL, in the form of https://login.microsoftonline.com/<tenant>
-        clientSecret:process.env.CLIENT_SECRET // Client secret generated from the app registration in Azure portal
-    },
-    system: {
-        loggerOptions: {
-            loggerCallback(loglevel, message, containsPii) {
-                console.log(message);
-            },
-            piiLoggingEnabled: false,
-            logLevel: msal.LogLevel.Verbose,
-        }
-    }
+async function getClientSecret() {
+  try {
+    const keyVaultName = process.env.KEY_VAULT_NAME;
+    const secretName = process.env.KEY_VAULT_SECRET_NAME;
+    const url = `https://${keyVaultName}.vault.azure.net`;
+
+    const credential = new DefaultAzureCredential();
+    const client = new SecretClient(url, credential);
+    const secret = await client.getSecret(secretName);
+    return secret.value;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-const cca = new msal.ConfidentialClientApplication(msalConfig);
+async function initalize() {
+
+  const msalConfig = {
+    tenant: process.env.TENENT_ID,
+    auth: {
+      clientId: process.env.CLIENT_ID, // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
+      authority: `https://login.microsoftonline.com/${process.env.TENENT_ID}`, // Full directory URL, in the form of https://login.microsoftonline.com/<tenant>
+      clientSecret: env ? await getClientSecret() : process.env.CLIENT_ID, // Fetch client secret when env is not undefined
+    },
+    system: {
+      loggerOptions: {
+        loggerCallback(loglevel, message, containsPii) {
+          console.log(message);
+        },
+        piiLoggingEnabled: false,
+        logLevel: msal.LogLevel.Verbose,
+      },
+    },
+  };
+
+  const cca = new msal.ConfidentialClientApplication(msalConfig);
+  return cca;
+}
+
+initalize();
+
 const scopes = [`api://${process.env.CLIENT_ID}/api.readwrite`];
 
-const frontendUrl = process.env.FRONTEND_URL
+const frontendUrl = process.env.FRONTEND_URL;
 const redirectionUrl = frontendUrl + "/auth";
 const postLogoutRedirectionurl = frontendUrl + "/auth";
 const graphMeEndpoint = "https://graph.microsoft.com" + "v1.0/me";
-const frontendBaseUrl = frontendUrl
+const frontendBaseUrl = frontendUrl;
 
 module.exports = {
-    redirectionUrl,
-    postLogoutRedirectionurl,
-    graphMeEndpoint,
-    scopes,
-    frontendBaseUrl,
-    cca
+  redirectionUrl,
+  postLogoutRedirectionurl,
+  graphMeEndpoint,
+  scopes,
+  frontendBaseUrl,
+  cca: initalize(),
 };
