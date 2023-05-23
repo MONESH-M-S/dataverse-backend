51585988d2a3bc51bed86616f33750a22f814681
Dinesh <dineshkumar.p@unilever.com>
2023-05-23 17:05:44 +0530

    Updated Filename in product unproccessed

--- src/constants/columns/productUnprocessedColumns.js
+++ src/constants/columns/productUnprocessedColumns.js
@@ -1,6 +1,6 @@
 module.exports = [
   { header: "ID", key: "Id", width: 10 },
-  { header: "File Name", key: "FileName", width: 40 },
+  { header: "File Name", key: "Filename", width: 40 },
   { header: "Tag", key: "Tag", width: 40 },
   { header: "External Description", key: "Externaldesc", width: 30 },
   { header: "Created On", key: "Createdon", width: 10 },
