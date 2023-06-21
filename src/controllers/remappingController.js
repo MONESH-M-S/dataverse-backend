bda5de43730c4d9ee5f3a00cfd4c515659a2e683
Yogeswaran Sridhar <yogeswaran.sridhar@rootquotient.com>
2023-06-21 12:12:57 +0530

    Remapping: remove updation of scenario flag to SC 0 in product dimension

--- src/controllers/remappingController.js
+++ src/controllers/remappingController.js
@@ -162,7 +162,6 @@ const updateRemappingProductValues = async (req, res, next) => {
     const { id } = req.params;
     const updatedValues = req.body;
     updatedValues["Flag"] = "MM";
-    updatedValues["Scenarioflag"] = "SC 0";
 
     const updatedFile = await SmartMappingDetailsModel.update(updatedValues, {
       where: { id },
