44e8fb2be62f2a9fc08e9652b2a4b10a072487a4
Dinesh <dineshkumar.p@unilever.com>
2023-07-28 17:34:48 +0530

    Created Api for Fact Metadata

---
+++ src/models/FactMetadata.model.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/sequelize.config");
+
+const FactMetadata = sequelize.define(
+  "NielsenFactMetadata",
+  {
+    Fact_ID: { type: DataTypes.NUMBER, primaryKey: true, autoIncrement: true },
+    UniqueTag: DataTypes.STRING,
+    TagLong: DataTypes.STRING,
+    TAG_Internal: DataTypes.STRING,
+    SHORT_Internal: DataTypes.NUMBER,
+    LONG_Internal: DataTypes.STRING,
+    Cell: DataTypes.STRING,
+    CountryName: DataTypes.STRING,
+    NielsenMarketName: DataTypes.STRING,
+    Fact_Type_Internal: DataTypes.STRING,
+    GOOGLETRANS: DataTypes.STRING,
+    HarmonisedAttribute: DataTypes.STRING,
+    GlobalHarmonisedAttribute: DataTypes.STRING
+  },
+  {
+    tableName: "NielsenFactMetadata",
+    schema: "metadata",
+    timestamps: false,
+  }
+);
+
+module.exports = FactMetadata;
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -4,11 +4,21 @@ const {
   fetchSmlPcatRecordsPagination,
   updateSmlPcatRecords,
 } = require("../controllers/adminController");
+const {
+  fetchFactMetadataRecords,
+  fetchFactMetadataRecordsPagination,
+  updateFactMetadataRecords,
+} = require("../controllers/admin/factMetadata");
 const router = express.Router();
 const auth = require("../middlewares/auth.middleware");
 
 router.get("/sml-pcat", auth, fetchSmlPcatRecords);
 router.get("/sml-pcat/count", auth, fetchSmlPcatRecordsPagination);
 router.put("/sml-pcat", auth, updateSmlPcatRecords);
+
+// Fact Metadata
+router.get("/fact", auth, fetchFactMetadataRecords);
+router.get("/fact/count", auth, fetchFactMetadataRecordsPagination);
+router.put("/fact", auth, updateFactMetadataRecords);
 
 module.exports = router;
