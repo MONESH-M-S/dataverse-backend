8bce1c8e64251e86ffb7da43417fcda55b0e126e
dinesh <dineshkumar.p@uniliver.com>
2023-08-10 14:50:54 +0530

    Refactor POS product code

---
+++ src/controllers/SmartMapping/POS/Product/ProductMappingPOSController.js
@@ -0,0 +1,78 @@
+const ProductMappingPOSModel = require('../../../../models/SmartMapping/POS/Product/ProductDetailPOS.model')
+const getPaginationDetails = require('../../../../utils/response/getPaginationDetails')
+const sendAsExcelFile = require('../../../../utils/response/sendAsExcelFile')
+const productMappedColumns= require('../../../../constants/Excel-Columns/SmartMapping/POS/Product/posProductMappedColumns')
+
+
+const fetchProductPOSMapping = async (req, res, next) => {
+    try {
+        const { limit, offset } = getPaginationDetails(req);
+        const { Filename } = req.query;
+
+        const whereClause = {
+            Filename: Filename
+        };
+
+        const result = await ProductMappingPOSModel.findAll({
+            limit,
+            offset,
+            where: whereClause,
+        });
+
+        res.json({ result: result });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const fetchProductPOSMappingPagination = async (req, res, next) => {
+    try {
+        const { limit, offset, page, pageSize } = getPaginationDetails(req);
+        const { Filename } = req.query;
+
+        const whereClause = {
+            Filename: Filename
+        };
+
+        const count = await ProductMappingPOSModel.count({
+            limit,
+            offset,
+            where: whereClause,
+        });
+
+        const responseObj = {
+            page,
+            page_size: pageSize,
+            total_count: count,
+        };
+
+        res.json(responseObj);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const downloadProductPOSMapping = async (req, res, next) => {
+    try {
+        const { Filename } = req.query;
+
+        const table = {};
+
+        const whereClause = {
+            Filename: Filename,
+        };
+
+        table.model = ProductMappingPOSModel;
+        table.columns = productMappedColumns;
+
+        const data = await table.model.findAll({
+            where: whereClause,
+        });
+
+        sendAsExcelFile(res, table, Filename, data);
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { fetchProductPOSMapping, fetchProductPOSMappingPagination, downloadProductPOSMapping }
--- src/routes/SmartMapping/product.routes.js
+++ src/routes/SmartMapping/product.routes.js
@@ -4,14 +4,27 @@ const router = express.Router()
 const { fetchProductMapping, fetchProductMappingPagination, downloadProductMapping } = require('../../controllers/SmartMapping/Nielsen/Product/ProductMappingController')
 const { fetchProductUnprocessed, fetchProductUnprocessedPagination, downloadProductUnproccessed } = require('../../controllers/SmartMapping/Nielsen/Product/ProductUnprocessedController')
 
+const { fetchProductPOSMapping, fetchProductPOSMappingPagination, downloadProductPOSMapping } = require('../../controllers/SmartMapping/POS/Product/ProductMappingPOSController')
+const { fetchProductPOSUnprocessed, fetchProductPOSUnprocessedPagination, downloadProductPOSUnprocessed } = require('../../controllers/SmartMapping/POS/Product/ProductUnprocessedPOSController')
+
 //Nielsen Product Mapped
 router.get('/mapped', fetchProductMapping)
 router.get('/mapped/count', fetchProductMappingPagination)
 router.get('/mapped/download', downloadProductMapping)
 
+//POS Product Mapped
+router.get('/pos/mapped', fetchProductPOSMapping)
+router.get('/pos/mapped/count', fetchProductPOSMappingPagination)
+router.get('/pos/mapped/download', downloadProductPOSMapping)
+
 //Nielsen Product UnProcessed
 router.get('/unprocessed', fetchProductUnprocessed)
-router.get('/unprocessed/count', fetchProductUnprocessedPagination);
+router.get('/unprocessed/count', fetchProductUnprocessedPagination)
 router.get('/unprocessed/download', downloadProductUnproccessed)
+
+//POS Product UnProcessed
+router.get('/pos/unprocessed', fetchProductPOSUnprocessed)
+router.get('/pos/unprocessed/count', fetchProductPOSUnprocessedPagination)
+router.get('/pos/unprocessed/download', downloadProductPOSUnprocessed)
 
 module.exports = router
