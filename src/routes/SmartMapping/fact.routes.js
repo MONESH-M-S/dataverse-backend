e147ef2d760d9dfce7adf4ff58032639a5a20b5c
dinesh <dineshkumar.p@uniliver.com>
2023-08-10 23:30:26 +0530

    Refactor POS Fact code

--- src/routes/SmartMapping/fact.routes.js
+++ src/routes/SmartMapping/fact.routes.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router()
 const { fetchFactMapping, fetchFactMappingPagination, downloadFactMapping } = require('../../controllers/SmartMapping/Nielsen/Fact/FactMappingController')
 const { fetchFactUnprocessed, fetchFactUnprocessedPagination, downloadFactUnprocessed } = require('../../controllers/SmartMapping/Nielsen/Fact/FactUnprocessedController')
+const { fetchMarketPOSMapping, fetchMarketPOSMappingPagination, downloadPosMarketMapping } = require('../../controllers/SmartMapping/POS/Fact/FactMappingPOSController')
 
 //Nielsen Fact Mapped
 router.get('/mapped', fetchFactMapping);
 router.get('/mapped/count', fetchFactMappingPagination)
 router.get('/mapped/download', downloadFactMapping);
 
+//POS Fact Mapped
+router.get('/pos/mapped', fetchMarketPOSMapping);
+router.get('/pos/mapped/count', fetchMarketPOSMappingPagination);
+router.get('/pos/mapped/download', downloadPosMarketMapping);
 
 //Nielsen Fact Unproccessed
 router.get('/unprocessed', fetchFactUnprocessed);
