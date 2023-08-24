adf7b838c838975ddba54f79e059afcb2926bc83
Dinesh <dineshkumar.p@unilever.com>
2023-08-24 09:59:42 +0530

    updated credential creation for CA pipeline trigger Api

--- src/controllers/FileVolatility/CriticalAttributes.js
+++ src/controllers/FileVolatility/CriticalAttributes.js
@@ -1,5 +1,5 @@
 const { DataFactoryManagementClient } = require("@azure/arm-datafactory");
-const { ManagedIdentityCredential } = require("@azure/identity");
+const { ClientSecretCredential, DefaultAzureCredential, InteractiveBrowserCredential } = require("@azure/identity");
 const {getClientSecret} = require('../../config/msal.config')
 
 const triggerADFPipeline = async (req, res, next) => {
@@ -19,9 +19,10 @@ const triggerADFPipeline = async (req, res, next) => {
       parameters,
     };
 
-    // const credential = new DefaultAzureCredential();
-    const credential = new ManagedIdentityCredential(process.env["CLIENT_ID"])
-
+    const credential = new InteractiveBrowserCredential(process.env["TENENT_ID"], process.env["CLIENT_ID"]);
+    // const credential = new ManagedIdentityCredential(process.env["CLIENT_ID"])
+    // const credential = new ClientSecretCredential(, , "Y5t8Q~YWA1s9sT6ToOqA1NH3aVLhZoBIfG4RMaaz")
+    console.log(credential);
     const client = new DataFactoryManagementClient(credential, subscriptionId);
 
     const result = await client.pipelines.createRun(
