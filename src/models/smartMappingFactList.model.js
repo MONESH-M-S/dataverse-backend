b9bdcdfd1d6430f3617728fb2b3ed6ca10446231
dinesh <dineshkumar.p@unilever.com>
2023-09-13 10:09:10 +0530

    Smartmapping model Updated

---
+++ src/models/smartMappingFactList.model.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/sequelize.config");
+const TempManualMappingModel = require("./tempManualMapping.model");
+
+const SmartMappingFactListModel = sequelize.define(
+  "vw_MappingReportSummary",
+  {
+    Id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    ExternalDataProvider: DataTypes.STRING,
+    Filename: DataTypes.STRING,
+    TotalRecords: DataTypes.STRING,
+    TotalMappedRecords: DataTypes.STRING,
+    TotalUnprocessedRecords: DataTypes.STRING,
+    High: DataTypes.STRING,
+    Medium: DataTypes.STRING,
+    Low: DataTypes.STRING,
+    TotalRecordsMappedBySME: DataTypes.STRING,
+    CreatedOn: DataTypes.DATE,
+    Country: DataTypes.STRING,
+    Category: DataTypes.STRING,
+    Dimension: DataTypes.STRING,
+  },
+  {
+    tableName: "vw_MappingReportSummary",
+    schema: "Mapping",
+    timestamps: false,
+  }
+);
+
+SmartMappingFactListModel.hasOne(TempManualMappingModel, {
+  foreignKey: "MappingOutputId",
+});
+
+module.exports = SmartMappingFactListModel;
