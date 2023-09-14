fd335f82f8e36f2c75e161901e1c596514ecc695
Dinesh <dineshkumar.p@unilever.com>
2023-09-14 14:43:41 +0530

    updated unprocessed remapping Api

--- src/controllers/Remapping/Nielsen/ProductRemappingController.js
+++ src/controllers/Remapping/Nielsen/ProductRemappingController.js
@@ -1,123 +1,123 @@
-const { Sequelize } = require("../../../../models");
+const { Sequelize, sequelize } = require("../../../../models");
 const { Op } = require("sequelize");
 const productUnprocessedModel = require("../../../models/SmartMapping/Nielsen/Product/ProductUnproccessed.model");
-const { Product_Dropdowns } = require('../../../constants/Remapping/remappingConstant')
+const {
+  Product_Dropdowns,
+} = require("../../../constants/Remapping/remappingConstant");
 const statusTypeEnum = require("../../../enums/statusType.enum");
 
 const getWhereObjectFromQuery = (query) => {
-    let whereClause = {};
+  let whereClause = {};
 
-    Object.keys(query).forEach((key) => {
-        if (key === "PeriodStartDate") {
-            whereClause[key] = {
-                [Op.gte]: query[key],
-            };
-        } else if (key === "PeriodEndDate") {
-            whereClause[key] = {
-                [Op.lte]: query[key],
-            };
-        } else {
-            whereClause[key] = query[key];
-        }
-    });
+  Object.keys(query).forEach((key) => {
+    if (key === "PeriodStartDate") {
+      whereClause[key] = {
+        [Op.gte]: query[key],
+      };
+    } else if (key === "PeriodEndDate") {
+      whereClause[key] = {
+        [Op.lte]: query[key],
+      };
+    } else {
+      whereClause[key] = query[key];
+    }
+  });
 
-    return whereClause;
+  return whereClause;
 };
-
 
 const productRemappingOptions = async (req, res, next) => {
-    try {
-        const whereClause = getWhereObjectFromQuery(req.query);
+  try {
+    const whereClause = getWhereObjectFromQuery(req.query);
 
-        const columnName = req.params.columnName;
-        const dbColumnName = Product_Dropdowns[columnName];
-        const options = await ProductMappingModel.findAll({
-            attributes: [
-                [Sequelize.fn("DISTINCT", Sequelize.col(dbColumnName)), "name"],
-            ],
-            where: whereClause,
-        });
+    const columnName = req.params.columnName;
+    const dbColumnName = Product_Dropdowns[columnName];
+    const options = await ProductMappingModel.findAll({
+      attributes: [
+        [Sequelize.fn("DISTINCT", Sequelize.col(dbColumnName)), "name"],
+      ],
+      where: whereClause,
+    });
 
-        res.json(options);
-    } catch (error) {
-        next(error);
-    }
+    res.json(options);
+  } catch (error) {
+    next(error);
+  }
 };
 
-
 const updateRemappingProductValues = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const updatedValues = req.body;
+    updatedValues["Flag"] = "MM";
+    updatedValues["Confidencelevel"] = "HIGH";
+    updatedValues["Confidencescore"] = "1";
 
-    try {
-        const { id } = req.params;
-        const updatedValues = req.body;
-        updatedValues["Flag"] = "MM";
-        updatedValues["Confidencelevel"] = "HIGH";
-        updatedValues["Confidencescore"] = "1";
+    const updatedFile = await ProductMappingModel.update(updatedValues, {
+      where: {
+        id,
+      },
+    });
 
-        const updatedFile = await ProductMappingModel.update(updatedValues, {
-            where: {
-                id
-            },
-        });
-
-        res.json({
-            status: statusTypeEnum.success,
-            message: `Successfully updated ${updatedFile[0]} record!`,
-        });
-    } catch (error) {
-        next(error);
-    }
+    res.json({
+      status: statusTypeEnum.success,
+      message: `Successfully updated ${updatedFile[0]} record!`,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const productRemappingUnprocessedOptions = async (req, res, next) => {
-    try {
-        const whereClause = getWhereObjectFromQuery(req.query);
+  try {
+    const whereClause = getWhereObjectFromQuery(req.query);
 
-        const columnName = req.params.columnName;
-        const dbColumnName = Product_Dropdowns[columnName];
-        const options = await productUnprocessedModel.findAll({
-            attributes: [
-                [Sequelize.fn("DISTINCT", Sequelize.col(dbColumnName)), "name"],
-            ],
-            where: whereClause,
-        });
+    const columnName = req.params.columnName;
+    const dbColumnName = Product_Dropdowns[columnName];
+    const options = await productUnprocessedModel.findAll({
+      attributes: [
+        [Sequelize.fn("DISTINCT", Sequelize.col(dbColumnName)), "name"],
+      ],
+      where: whereClause,
+    });
 
-        res.json(options);
-
-    } catch (error) {
-        next(error)
-    }
-}
+    res.json(options);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const updateRemappingUnprocessedProductOptions = async (req, res, next) => {
+  try {
+    const updatedValues = req.body;
 
-    try {
-        const { id } = req.params;
-        const updatedValues = req.body;
-        updatedValues["Flag"] = "MM";
-        updatedValues["Confidencelevel"] = "HIGH";
-        updatedValues["Confidencescore"] = "1";
+    let query = "";
 
-        const updatedFile = await productUnprocessedModel.update(updatedValues, {
-            where: {
-                id
-            },
-        });
-
-        res.json({
-            status: statusTypeEnum.success,
-            message: `Successfully updated ${updatedFile[0]} record!`,
-        });
-
-    } catch (error) {
-        next(error)
+    if (Object.values(updatedValues).length) {
+      Object.values(updatedValues).forEach((value, index) => {
+        if (index === 0) query += `'${value}'`;
+        else query += `, '${value}'`;
+      });
     }
-}
 
+    const updatedFile = sequelize.query(`
+        exec [Mapping].[spRemappingUnprocessed] ${query.length ? query : ""}
+    `);
+
+    console.log("Unprocessed remapping", updatedFile);
+
+    res.json({
+      status: statusTypeEnum.success,
+      message: `Successfully updated!`,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
-    productRemappingOptions,
-    updateRemappingProductValues,
-    productRemappingUnprocessedOptions,
-    updateRemappingUnprocessedProductOptions
-}
+  productRemappingOptions,
+  updateRemappingProductValues,
+  productRemappingUnprocessedOptions,
+  updateRemappingUnprocessedProductOptions,
+};
