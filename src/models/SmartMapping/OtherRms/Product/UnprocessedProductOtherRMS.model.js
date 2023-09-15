3cdb0fbd7deed9415b9b2b28872806a11a0af5cb
dinesh <dineshkumar.p@uniliver.com>
2023-09-15 10:54:26 +0530

    Updated folder in otherrms model based on dimension and Created Unprocessed model for product in otherRMS

---
+++ src/models/SmartMapping/OtherRms/Product/UnprocessedProductOtherRMS.model.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../../../../config/sequelize.config");
+
+const UnprocessedProductOtherRMSModel = sequelize.define(
+  "UnProcessedRecordsProduct_RMS",
+  {
+    Id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Filename: DataTypes.STRING,
+    Tag: DataTypes.STRING,
+    Externaldesc: DataTypes.STRING,
+    Createdon: DataTypes.STRING,
+    Remark: DataTypes.STRING,
+  },
+  {
+    tableName: "UnProcessedRecordsProduct_RMS",
+    schema: "Mapping",
+    timestamps: false,
+  }
+);
+
+module.exports = UnprocessedProductOtherRMSModel;
+
+ 
